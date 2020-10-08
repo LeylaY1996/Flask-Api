0ce438ca3145b59ca433dab00dd6f2100863e79e
Leyla <leylay1996@gmail.com>
2020-10-09 02:37:49 +0300

    update method problem

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_mysqldb import MySQL
-from flask import jsonify # <- `jsonify` instead of `json`
+from flask import jsonify,json # <- `jsonify` instead of `json`
 
 
 app = Flask(__name__)
@@ -12,7 +12,8 @@ app.config['MYSQL_DB'] = 'python_api'
 
 mysql = MySQL(app)
 
-@app.route('/add_users' , methods=['POST'])
+@app.route('/add_user' , methods=['POST'])
+
 def add_user():
 
    details = request.form
@@ -27,9 +28,30 @@ def add_user():
    return 'Kullanıcı Eklendi'
 
 @app.route('/get_users', methods=['GET'])
+
 def get_users():
+
    cur = mysql.connection.cursor()
    cur.execute("SELECT * FROM users")
-   rows = cur.fetchall()
-   resp = jsonify(rows)
-   return resp
+   rows = jsonify(cur.fetchall())
+   return rows
+
+@app.route('/get_user/<id>')
+def get_user(id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users WHERE id=%s", id)
+    rows = jsonify(cur.fetchone())
+    return rows
+
+@app.route('/update_user', methods=['POST'])
+def update_user():
+   
+   details = request.form
+   _id = details['id']
+   firstname = details['firstname']
+   lastname = details['lastname']
+   email = details['email']
+   cur = mysql.connection.cursor()
+   cur.execute("UPDATE users(firstname,lastname,email) VALUES (%s,%s,%s)",(firstname,lastname,email))
+   mysql.connection.commit()
+   cur.close()
